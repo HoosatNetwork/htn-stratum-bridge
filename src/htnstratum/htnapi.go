9cb581b9504afcfc3ca27dc3d9e0e6905a7a372c
MattF42 <matt_foster@me.com>
2026-03-06 12:42:46 +0000

    Change log level from Info to Debug for cache hit rate

--- src/htnstratum/htnapi.go
+++ src/htnstratum/htnapi.go
@@ -235,7 +235,7 @@ func (htnApi *HtnApi) GetBlockTemplate(client *gostratum.StratumContext, poll in
 			total := hits + misses
 			if total%1000 == 0 {
 				rate := (float64(hits) / float64(total)) * 100.0
-				htnApi.logger.Infof("GBT cache hit rate %.2f%% (%d/%d), ttl=%s", rate, hits, total, htnApi.gbtCacheTTL)
+				htnApi.logger.Debug("GBT cache hit rate %.2f%% (%d/%d), ttl=%s", rate, hits, total, htnApi.gbtCacheTTL)
 			}
 			return cached, nil
 		}
